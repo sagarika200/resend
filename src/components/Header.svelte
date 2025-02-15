d815192a7c24a9ac7d621bfc8de1108d90b268dd
sagarika200 <sagarikahota2004@gmail.com>
2025-02-15 12:37:14 +0530

    Add files via upload

---
+++ src/components/Header.svelte
@@ -0,0 +1,58 @@
+<script>
+    export let y;
+
+    export let tabs = [
+        
+        { name: "About ", link: '/about' },
+        { name: "Blog", link: "#Blog" },
+        { name: "Customers ", link: '/customers' },
+        { name: "Resources ", link: "#Resources " },
+        { name: "Docs ", link: "#Docs" },
+        { name: "Pricing ", link: '/pricing' },
+        
+        { name: "Sign in ", link: "#Sign in" },
+        // { name: "Contact", link: "" },
+        // {name: '', link: ''},
+    ];
+</script>
+
+<header
+    class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
+        (y > 0
+        ? " py-4 bg-slate-950 border-violet-950"
+        : " py-6 bg-transparent border-transparent")}
+>
+<div>
+    <a href="http://localhost:5173/" class="button">Resend</a>
+    
+</div>
+   
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a
+                href={tab.link}
+                class="duration-200 hover:text-violet-400"
+                target={index === 2 ? "_blank" : ""}
+            >
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+        <!-- <button
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-violet-600 text-white"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Login</h4>
+        </button> -->
+        <button
+        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-3 py-3 group rounded-full bg-white text-slate-950"
+      >
+        <div
+          class=" top-0 right-full w-full h-full  group-hover:translate-x-full"
+        />
+        <a href="https://resend.com/signup" class="button">Get Started</a>
+      </button>
+        </div>
+    
+</header>
---
+++ src/routes/customers/+page.svelte
@@ -0,0 +1,233 @@
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section>
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <div>
+        <br />
+        <h1 class={' text-7xl sm:text-7xl text-center '}>Meet our Customers</h1>
+
+        <h6 class="text-large sm:text-xl md:text-2xl text-center">
+          <br />Companies of all sizes trust Resend to deliver their most
+          important emails.
+        </h6>
+      </div>
+
+      <div class="mx-auto mb-1 grid grid-cols-2 gap-3 md:flex">
+        <button
+          class="text-sm h-10 pl-3 pr-3 rounded-md gap-4 font-semibold bg-black dark:bg-white text-white dark:text-black border-slate-6 hover:bg-black/90 dark:hover:bg-white/90 focus-visible:ring-2 dark:focus-visible:ring-white/40 focus-visible:ring-black/40 focus-visible:outline-none dark:focus-visible:bg-white/90 focus-visible:bg-black/90 disabled:hover:bg-black dark:disabled:hover:bg-white inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+        >
+          <span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+            ><span class="inline-flex items-center gap-1"
+              ><span class="h-1 w-1 animate-plop rounded-full bg-root"> </span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-root"></span>
+              <span class="h-1 w-1 animate-plop3 rounded-full bg-root"></span>
+            </span>
+          </span>
+
+          <span
+            class="inline-flex items-center justify-center gap-2 truncate visible"
+            >Featured</span
+          >
+        </button>
+        <button
+          href="https://resend.com/customers?category=ai"
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+        >
+          <span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+            ><span class="inline-flex items-center gap-1"
+              ><span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span><span
+                class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span><span
+                class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              ></span></span
+            ></span
+          ><span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >AI</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+        >
+          <span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+            ><span class="inline-flex items-center gap-1"
+              ><span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              ></span>
+            </span></span
+          >
+          <span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Developer Tools</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+        >
+          <span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+            ><span class="inline-flex items-center gap-1"
+              ><span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span><span
+                class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              >
+              </span>
+            </span>
+          </span>
+          <span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Fintech</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+          ><span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+          >
+            <span class="inline-flex items-center gap-1">
+              <span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop3 rounded-full bg-slate-12">
+              </span>
+            </span>
+          </span>
+          <span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Healthcare</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+          ><span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+            ><span class="inline-flex items-center gap-1"
+              ><span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span><span
+                class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              ></span>
+            </span>
+          </span>
+          <span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Open Source
+          </span>
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+          ><span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+          >
+            <span class="inline-flex items-center gap-1">
+              <span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop3 rounded-full bg-slate-12">
+              </span>
+            </span>
+          </span>
+          <span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >SaaS</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+        >
+          <span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+          >
+            <span class="inline-flex items-center gap-1">
+              <span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              ></span></span
+            ></span
+          ><span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Studios</span
+          >
+        </button>
+        <button
+          class="text-sm h-10 pl-4 pr-4 rounded-md gap-1 font-semibold dark:bg-slate-3 border-slate-6 dark:text-slate-11 bg-slate-2 text-slate-12 hover:bg-slate-4 dark:hover:bg-slate-4 focus-visible:ring-2 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-4 disabled:hover:bg-slate-4 inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer block"
+          ><span
+            class="absolute w-full inset-0 flex items-center justify-center invisible"
+          >
+            <span class="inline-flex items-center gap-1">
+              <span class="h-1 w-1 animate-plop rounded-full bg-slate-12"
+              ></span>
+              <span class="h-1 w-1 animate-plop2 rounded-full bg-slate-12"
+              ></span><span
+                class="h-1 w-1 animate-plop3 rounded-full bg-slate-12"
+              ></span></span
+            ></span
+          ><span
+            class="inline-flex items-center justify-center gap-1 truncate visible"
+            >Travel</span
+          >
+        </button>
+      </div>
+      <div
+        class="mx-auto flex w-full max-w-5x1 flex-wrap gap-12 sm:grid sm:grid-cols-2 md:grid-cols-3"
+      >
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/anghami" class="button"
+            ><img src={'images/a.png'} alt="Company" /></a
+          >"
+        </div>
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/pokedoku" class="button"
+            ><img src={'images/b.png'} alt="Company" /></a
+          >"
+        </div>
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/payload" class="button"
+            ><img src={'images/c.png'} alt="Company" /></a
+          >"
+        </div>
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/liveblocks" class="button"
+            ><img src={'images/d.png'} alt="Company" /></a
+          >"
+        </div>
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/dub" class="button"
+            ><img src={'images/e.png'} alt="Company" /></a
+          >"
+        </div>
+        <div class="box-border h-80 w-60 p-5 border-2">
+          <a href="https://resend.com/customers/fey" class="button"
+            ><img src={'images/f.png'} alt="Company" /></a
+          >"
+        </div>
+      </div>
+      <div class="relative shadow-2x1 grid place-items-center">
+        <img
+          src={'images/g.png'}
+          alt="About"
+          class="object-cover z-[2] max-h-[100vh]"
+        />
+      </div>
+    </div>
+    <div></div>
+  </section>
+</main>
